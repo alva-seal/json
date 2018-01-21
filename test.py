071a7a07498760ae11c3697156b68e0e33b2d756
Fabian <github@ffabi.com>
2018-01-21 16:08:38 +0100

    removed testcase with need for live testing

--- test.py
+++ test.py
@@ -3,13 +3,6 @@ import requests
 from main import app
 
 
-class TestFlaskApiUsingRequests(unittest.TestCase):
-    def test_hello(self):
-        response = requests.get('http://localhost:5000')
-        print(response.content)
-        self.assertEqual(response.content.decode(), "Hello World from Python!")
-
-
 class TestFlaskApi(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
